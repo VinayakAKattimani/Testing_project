2746104f60ede34a3d948294b5af377678102974
Vinayak Kattimani <vinaykatimani2@gmail.com>
2025-12-26 21:15:59 +0530

    done

--- src/test/java/JavaJenkinsFirstProject/TestLogin.java
+++ src/test/java/JavaJenkinsFirstProject/TestLogin.java
@@ -1,2 +1,32 @@
-package JavaJenkinsFirstProject;public class TestLogin {
+package JavaJenkinsFirstProject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class TestLogin {
+    WebDriver driver =new ChromeDriver();
+
+    @BeforeClass
+    public void login(){
+        driver.get("https://demo.guru99.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.findElement(By.xpath("//*[@id=\"navbar-brand-centered\"]/ul/li[5]/a")).click();
+    }
+    @Test
+    public void isLoginPageDisplayed(){
+        Assert.assertTrue(driver.findElement(By.xpath("/html/body/div[2]/h2")).isDisplayed());
+    }
+
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
 }
